ef29d0e657ce3afc99677bc1bbde2b310ffae43e
Adam Wolski <wolas.abc@gmail.com>
2018-05-20 16:29:17 +0200

    getting wemark password via email

---
+++ features/2-feature.feature
@@ -0,0 +1,8 @@
+@watch
+Feature: Signing in with one time password
+	Scenario: Properly signing in
+		Given I have received the one time password
+		When I fill the password field
+		Then the button ".wemark-sign-up-button" should be active
+		When I click on ".wemark-sign-up-button"
+		Then I should be logged in and redirected 
---
+++ features/2-feature.js
@@ -0,0 +1,76 @@
+const POP3Client = require("poplib");
+const imaps = require('imap-simple');
+let imap_connection = null;
+
+imaps.connect(
+	{imap: {
+  user: 'wemtest8@gmail.com',
+  password: '',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true
+}
+}).then(function (connection) {
+    return connection.openBox('INBOX').then(function () {
+        imap_connection = connection;
+    });
+});
+
+
+module.exports = function() {
+	const TIMEOUT = 30000; //10 seconds
+	let pass = '';
+
+	this.Given(/^I have received the one time password$/, function () {
+    	client.waitUntil(function () {	
+    		if(pass) {
+    			return true;
+    		}
+
+    		if(!imap_connection) {
+    			return false;
+    		}
+
+    		imap_connection.search([
+            	'UNSEEN'
+	        ], {
+	            bodies: ['TEXT'],
+	            markSeen: true
+	        }).then(function (results) {
+	        	if(results) {
+		        	var bodies = results.map(function (res) {
+		                return res.parts.filter(function (part) {
+		                    return part.which === 'TEXT';
+		                })[0].body;
+	            	});
+
+		        	const wemarkEmail = bodies[0];
+
+		        	if(wemarkEmail) {
+		        		const passwordString = wemarkEmail.match(/password\:\s(\d{10})/);
+
+		        		if(passwordString[1]) {
+		        			pass = passwordString[1];
+		        		}
+		        	}
+	        	}
+	        });
+
+
+    		return !!pass;
+	    }, TIMEOUT, 'didnt receive password code', 2000);
+  	});
+
+  	this.When(/^I fill the password field$/, function () {
+    	client.waitForExist('#password', TIMEOUT);
+    	client.waitForVisible('#password', TIMEOUT);
+ 		client.setValue('#password', pass);
+    });
+
+    this.Then(/^I should be logged in and redirected$/, function () {
+        client.waitForVisible('#password', 10000, true);
+        client.waitForExist('.my-account-page', TIMEOUT);
+        client.waitForVisible('.my-account-page', TIMEOUT);
+
+    });
+}
